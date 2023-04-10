69a0bc35e4f052f02347360805a3d90f4e6eaecf
OleksandrMorozJito <OleksandrMorozJito@users.noreply.github.com>
2023-04-10 21:50:47 +0000

    Deploy to GitHub pages

---
+++ static/js/93.148313b1.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkjito_website=self.webpackChunkjito_website||[]).push([[93],{7093:function(e,n,t){t.r(n),t.d(n,{default:function(){return o}});t(7313);var i=t(2135),r=t(6417);var o=function(){return(0,r.jsx)("div",{children:(0,r.jsx)(i.OL,{to:"/tools/invoice-generator",children:"Invoice Generator"})})}}}]);
